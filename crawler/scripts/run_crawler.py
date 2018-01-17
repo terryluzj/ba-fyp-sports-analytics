4986539fe7d84a42c9da15deff30014ab71557eb
Terry Lu Zijian <terryluzijian@gmail.com>
2018-01-17 21:56:48 +0800

    Updated Crawler Code

---
+++ crawler/scripts/run_crawler.py
@@ -0,0 +1,23 @@
+import scrapy.crawler
+from scrapy.utils.project import get_project_settings
+
+
+class ProfileCrawlerProcess(scrapy.crawler.CrawlerProcess):
+
+    CRAWLER_NAME = 'core'
+
+    def __init__(self):
+        super(ProfileCrawlerProcess, self).__init__(settings=get_project_settings())
+
+    def start_crawl(self):
+        self.crawl(ProfileCrawlerProcess.CRAWLER_NAME)
+        self.start()
+
+
+def run_crawler():
+    process = ProfileCrawlerProcess()
+    process.start_crawl()
+
+
+if __name__ == '__main__':
+    run_crawler()
