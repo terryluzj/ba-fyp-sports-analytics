8d3c0be0fcecf9afe3e2378f1361cffc11865a0f
Terry Lu Zijian <terryluzijian@gmail.com>
2018-03-30 23:05:08 +0800

    Updated indentation and line breaks

--- analysis/predictive/rnn_model/load_data.py
+++ analysis/predictive/rnn_model/load_data.py
@@ -8,12 +8,14 @@ from analysis.predictive.rnn_model.settings import TRAINING_LABEL
 def read_feature_engineered_dataframe(drop_rank_info=True, filter_columns=True, reuse=True, **params):
     # Read from file df_combined_all.csv
     if reuse:
-        race_combined_featured = pd.read_csv('{}{}'.format(DATA_DIRECTORY_FEATURE_ENGINEERED, 'df_combined_all.csv'), index_col=0)
+        race_combined_featured = pd.read_csv('{}{}'.format(DATA_DIRECTORY_FEATURE_ENGINEERED, 'df_combined_all.csv'),
+                                             index_col=0)
         race_combined_featured['run_date'] = race_combined_featured['run_date'].apply(lambda x: pd.Timestamp(x))
         race_combined_featured.set_index(['horse_id', 'run_date'], inplace=True)
     else:
         # Do feature engineering on the dataframe if specified (pass params for options defined in the function)
-        race_combined_featured_tuple = feature_engineer(read_race_dataframe(**params, reset_index=True), df_name='df_combined_all_new')
+        race_combined_featured_tuple = feature_engineer(read_race_dataframe(**params, reset_index=True),
+                                                        df_name='df_combined_all_new')
         race_combined_featured = race_combined_featured_tuple[0]
         if not drop_rank_info:
             race_combined_featured['finishing_position'] = race_combined_featured_tuple[1]
@@ -26,14 +28,16 @@ def read_feature_engineered_dataframe(drop_rank_info=True, filter_columns=True, 
 
 def read_race_dataframe(filter_first_occurrence=True, filter_columns=True, reset_index=True):
     # Read from file horse_race_combined.csv
-    race_combined = pd.read_csv('{]{}'.format(DATA_DIRECTORY_PROCESSED, 'horse_race_combined.csv'), index_col=0)
+    race_combined = pd.read_csv('{]{}'.format(DATA_DIRECTORY_PROCESSED, 'horse_race_combined.csv'),
+                                index_col=0)
     race_combined['run_date'] = race_combined['run_date'].apply(lambda x: pd.Timestamp(x))
     race_combined = race_combined.sort_values(['horse_id', 'run_date'])
     race_combined.set_index(['horse_id', 'run_date'], inplace=True)
 
     if filter_first_occurrence:
         # Read data from those with first occurrence and drop some columns
-        first_occurrence_df = pd.read_csv('{]{}'.format(DATA_DIRECTORY_PROCESSED, 'first_occurrence_race.csv'), index_col=0)
+        first_occurrence_df = pd.read_csv('{]{}'.format(DATA_DIRECTORY_PROCESSED, 'first_occurrence_race.csv'),
+                                          index_col=0)
         first_occurrence_df['run_date'] = first_occurrence_df['run_date'].apply(pd.Timestamp)
         first_occurrence_df = first_occurrence_df.sort_values(['horse_id', 'run_date'])
         first_occurrence_df_index = first_occurrence_df.set_index(['horse_id', 'run_date']).index
@@ -51,6 +55,7 @@ def remove_dependent_columns(df):
     columns_to_drop = list(filter(lambda label: label != TRAINING_LABEL, DEPENDENT_COLUMNS))
     df.drop(columns_to_drop, axis=1, inplace=True)
     return columns_to_drop
+
 
 def train_test_split_by_date(df):
     pass
