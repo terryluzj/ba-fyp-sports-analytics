be66c5722c1155e0350a13e3d7f975ac5fa22a45
Terry Lu Zijian <terryluzijian@gmail.com>
2018-04-06 18:28:10 +0800

    Updated plotting in descriptive analysis notebook

--- analysis/predictive/rnn_model/settings.py
+++ analysis/predictive/rnn_model/settings.py
@@ -6,7 +6,7 @@ FILE_DIRECTORY = '{}\\'.format(os.path.abspath(os.path.join(os.path.dirname(os.p
 DATA_DIRECTORY = '{}{}\\'.format(FILE_DIRECTORY, 'data')
 
 # Feature and labels
-TRAINING_LABEL = 'y_{}'.format('run_time_diff')
+TRAINING_LABEL = 'y_{}'.format('run_time_ma_window_3_quo')
 PREDICT_FIRST_RACE = False
 
 # Logging
